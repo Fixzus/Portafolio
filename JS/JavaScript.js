0ed1336d256a78424929d51588f7039c08c24767
Fixzus <fixzussk@gmail.com>
2024-08-21 22:08:35 -0400

    casi casi listo...

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -9,10 +9,22 @@ function myFunction() {
 }
 function contacto(){
     const name = document.getElementById("nombre").value;
-    if (name!==""){
+    const apell = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const motivo = document.getElementById("motivo").value;
+    const inform = document.getElementById("informacion").value;
+
+    if (name === "" || apell === "" || email === "" || motivo === "0" || inform === ""){
+        alert("complete todos los campos");
+        scrip.stop();
+    }if(motivo=== "1") {
+        alert("               "+name +"\n"+" Tu email ha sido enviado,"+"\n"+"En breve te responderemos");
+    }if (motivo=== "2") {
+        alert("               "+name +"\n"+"gracias por tus comentarios,"+"\n"+"          los analizaremos");
+    }if (motivo=== "3") {
         alert("               "+name +"\n"+" Tu email ha sido enviado,"+"\n"+"En breve te responderemos");
     }else {
-        alert("complete los campos");
+        stop();
     }
 
 }
