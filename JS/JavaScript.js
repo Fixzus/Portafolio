aec484976094a60048b86a92a2ecd04314900ede
Fixzus Schaimee Kry <37422145+Fixzus@users.noreply.github.com>
2024-08-22 20:26:36 -0400

    Update JavaScript.js
    tenia error en el texto

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -22,7 +22,7 @@ function contacto(){
     }if (motivo=== "2") {
         alert("               "+name +"\n"+"gracias por tus comentarios,"+"\n"+"          los analizaremos");
     }if (motivo=== "3") {
-        alert("               "+name +"\n"+"  Lamentamos lo ocurrido,"+"\n"+"En breve te solucionaremos");
+        alert("               "+name +"\n"+"  Lamentamos lo ocurrido,"+"\n"+"En breve lo solucionaremos");
     }else {
         stop();
     }
